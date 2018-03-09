7382bdaa6a2285e55a5426c290593548dac20522
asdas <asdas@Newroz-PC>
2018-03-09 18:27:09 +0100

    doc added

--- Experimente/src/experimente/Farbe.java
+++ Experimente/src/experimente/Farbe.java
@@ -1,16 +1,37 @@
 package experimente;
 
+/**
+ * Diese Klasse dient dazu, Farbobjekte zu erstellen / beschreiben.
+ * 
+ * @author asdas
+ * @since V1.0
+ */
 public class Farbe {
+	
+	/**
+	 * Die Konstante fuer die Farbe "rot".
+	 */
+	public static final String rot = "red";
+	
+	/**
+	 * Instanzvariable, die den Farbwert haelt.
+	 */
 	private String farbwert;
 	
+	/**
+	 * Konstruktor
+	 * 
+	 * @param wert Ein String, welcher die Farbe beschreibt. Z.B. <code>rot</code>.
+	 */
 	public Farbe(String wert) {
 		farbwert = wert;
 	}
+	
 	/**
-	 * test
-	 * @return
+	 * Liefert den Farbwert als String.
+	 * 
+	 * @return Farbwert als String
 	 */
-
 	public String getFarbwert() {
 		return farbwert;
 	}
